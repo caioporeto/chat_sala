5f67f2e4fbc966112a3cc69f727398a001d0cc6a
caioporeto <caioporeto@gmail.com>
2024-12-09 16:33:15 -0300

    gpio integrated to tx and rx

--- rx/src/main.c
+++ rx/src/main.c
@@ -6,17 +6,30 @@
 
 #define PIN_NUMBER 3 // Substitua pelo número do pino
 #define GPIO_NODE DT_NODELABEL(gpiob) // Controlador GPIO (troque por "gpioa" ou outro conforme necessário)
+#define pin_tx DT_ALIAS(sw0)
 
 const struct device *pino_RX = DEVICE_DT_GET(GPIO_NODE);
+static const struct gpio_dt_spec pino_TX = GPIO_DT_SPEC_GET(pin_tx, gpios);
 
 int buffer;
 int bit_RX;
 
 void RX(void){
-	gpio_pin_configure(pino_RX, PIN_NUMBER, GPIO_INPUT);
+	gpio_pin_configure(pino_RX, PIN_NUMBER, GPIO_INPUT); // define RX como input
+	
+	gpio_pin_set_dt(&pino_TX, 0);
+	
 	bit_RX = gpio_pin_get(pino_RX , PIN_NUMBER);
 
 	buffer = ((buffer << 1) | bit_RX) & 0b1111;	
+	if(bit_RX == 0b1){
+		printk("1");
+	}
+	
+	else if(bit_RX == 0b0){
+		printk("0");
+	}
+
 }
 
 K_TIMER_DEFINE(RX_timer, RX, NULL);
@@ -24,6 +37,6 @@ K_TIMER_DEFINE(RX_timer, RX, NULL);
 int main(){
 	
 	k_timer_start(&RX_timer, K_USEC(2500), K_USEC(2500));
-	k_timer_start(&TX_timer, K_MSEC(10), K_MSEC(10));
+	int ret = gpio_pin_configure_dt(&pino_TX, GPIO_OUTPUT_ACTIVE);
 
 }
