60314f951da522b5e6d404e7973b67af870e3350
caioporeto <caioporeto@gmail.com>
2024-12-20 16:34:35 -0300

    final code - comments and working

--- tx&rx/src/chat_sala.c
+++ tx&rx/src/chat_sala.c
@@ -23,13 +23,10 @@
 #define MY_PRIORITY 0
 #define MSG_SIZE 8
 
-K_MUTEX_DEFINE(mutex_TX_RX);
 K_MUTEX_DEFINE(mutex_RX_Buffer);
 K_MUTEX_DEFINE(pode_transmitir);
 K_MUTEX_DEFINE(mymutex);
 
-
-K_CONDVAR_DEFINE(cond_RX_TX);
 K_CONDVAR_DEFINE(cond_RX_Buffer);
 K_CONDVAR_DEFINE(mycondvar);
 
@@ -41,15 +38,15 @@ static const struct gpio_dt_spec pino_TX = GPIO_DT_SPEC_GET(pin_tx, gpios);
 static const struct device *const uart_dev = DEVICE_DT_GET(UART_DEVICE_NODE);
 
 
-uint8_t Buffer = 0;
-uint32_t recebido_RX = 0;
+uint8_t Buffer = 0; //buffer em que será alocado os bytes que forem válidos
+uint32_t recebido_RX = 0; // lerá 32 bits (8*4)
 int reenvia = 0; // se algum byte enviado não for o esperado, reenviar a mensagem. Temos que implementar no codigo do TX uma forma de ele, toda vez que terminar de enviar o pacote, voltar essa variável para 0;
 int startbit = 0; // variável auxiliar para sinalizar a leitura do startbit
 int bit_RX;
 int x = 0; //contador RX
 int i = 0;
-int k_bit = 97;
-int tx;
+int k_bit; 
+int tx; //valor enviado ao pino tx
 static char rx_buf[MSG_SIZE];
 static int rx_buf_pos;
 int mensagem_enviada = 1;
@@ -69,19 +66,19 @@ struct package {
 
 static struct package pacote_recebido;
 static struct package pacote_enviado = {.U = 0b01010101, .sync = 0b00010110, .STX = 0b00000010, .id = 0b01010000, .end = 0b00010111};
-char id_random = 0b10101001;
 
-void montaBuffer(){
+char id_random = 0b10101001; // id utilizado para testar quando receber mensagens de outra pessoa
+
+void montaBuffer(){ // monta um buffer ao encontrar um byte valido
 
 	for(int cont = 8; cont >= 1; cont--){
 			
-		Buffer = ((Buffer << 1) | ( (recebido_RX >> ((cont*4) - 3)) & 0b1)); // monta o buffer
+		Buffer = ((Buffer << 1) | ( (recebido_RX >> ((cont*4) - 3)) & 0b1)); // shifta o buffer 1 vez e faz a mascara com um dos bits do meio dos 4 lidos repetidamente
 	
 	}
 
 }
 
-int bits = 0;
 int p = 0;
 int m = 0;
 int pin; 
@@ -91,29 +88,27 @@ void RX (void){
 	struct package *recebido = &pacote_recebido;
 	struct package *enviado = &pacote_enviado;
 
-	gpio_pin_configure(pino_RX, PIN_NUMBER, GPIO_INPUT); // define RX como input
+	gpio_pin_configure(pino_RX, PIN_NUMBER, GPIO_INPUT); // define o pino RX como input
 
-	pin = !(gpio_pin_get(pino_RX , PIN_NUMBER));
+	pin = !(gpio_pin_get(pino_RX , PIN_NUMBER)); // nega o valor recebido, pois recebe inverso do transmitido
 
-	k_condvar_signal(&cond_RX_Buffer);
+	k_condvar_signal(&cond_RX_Buffer); // envia um sinal a thread para receber o bit lido (sincronização)
 
 }
 
 void RXBuffer(){
 	struct package *recebido = &pacote_recebido;
-	struct package *enviado = &pacote_enviado;
+	struct package *enviado = &pacote_enviado; 
 
 	while(1){
 
-		k_condvar_wait(&cond_RX_Buffer, &mutex_RX_Buffer, K_FOREVER);
+		k_condvar_wait(&cond_RX_Buffer, &mutex_RX_Buffer, K_FOREVER); // espera um sinal do timer para receber o bit lido (sincronização)
 
-		recebido_RX = (recebido_RX << 1) | pin; 
+		recebido_RX = (recebido_RX << 1) | pin; // shifta 1 pro lado e faz mascara com valor lidos
 
-		bits++;
 		p++;
-
-		printk("%d", pin);
-		if(((recebido_RX >> 1) & 0b1) == ((recebido_RX >> 2) & 0b1) &&
+ 
+		if(((recebido_RX >> 1) & 0b1) == ((recebido_RX >> 2) & 0b1) && // condição que verifica se recebeu um byte é valido
 		((recebido_RX >> 5) & 0b1) == ((recebido_RX >> 6) & 0b1) &&
 		((recebido_RX >> 9) & 0b1) == ((recebido_RX >> 10) & 0b1) &&
 		((recebido_RX >> 13) & 0b1) == ((recebido_RX >> 14) & 0b1) &&
@@ -121,111 +116,102 @@ void RXBuffer(){
 		((recebido_RX >> 21) & 0b1) == ((recebido_RX >> 22) & 0b1) &&
 		((recebido_RX >> 25) & 0b1) == ((recebido_RX >> 26) & 0b1) &&
 		((recebido_RX >> 29) & 0b1) == ((recebido_RX >> 30) & 0b1)) { 
-			//printk("byte valido");
 		
-			montaBuffer();
+			montaBuffer(); // monta o buffer com os bits recebidos 
 
-			if((Buffer == enviado->U) && (recebido->U != enviado->U)){
-				recebido->U = Buffer;
-				k_mutex_lock(&pode_transmitir, K_NO_WAIT);
-				printk("U");
-			
-				p=0;
+			if((Buffer == enviado->U) && (recebido->U != enviado->U)){ // verifica se o buffer é igual ao pre e se ainda nao foi recebido o pre 
+				
+				recebido->U = Buffer; 
+
+				k_mutex_lock(&pode_transmitir, K_NO_WAIT); //tenta travar o mutex para caso alguem esteja transmitindo	
+				
+				p=0; 
 			}
 
-			if(recebido->U == enviado->U){		
+			if(recebido->U == enviado->U){ //verifica se ja recebeu o pre
 			
-				if(((Buffer == enviado->sync) && (p==32) && (recebido->sync != enviado->sync))){
+				if(((Buffer == enviado->sync) && (p==32) && (recebido->sync != enviado->sync))){ //verifica se o proximo byte pós pre lido é igual ao sync
 					recebido->sync = Buffer;
-					printk("sync");
 					p=0;
 				}
 
 				if(recebido->sync == enviado->sync){
-					if((Buffer == enviado->STX && (p==32) && (recebido->STX != enviado->STX))){
-						printk("STX");
+					if((Buffer == enviado->STX && (p==32) && (recebido->STX != enviado->STX))){ //verifica se o proximo byte pós sync lido é igual ao STX
 						recebido->STX = Buffer;
 						p=0;
 					}
 
 					if((recebido->STX == enviado->STX)){
 
-						if((p==32) && (Buffer != enviado->id) && (recebido->id != enviado->id)){
-							recebido->id = Buffer;
-							recebido->n = recebido->id & 0b111;
-							printk("id não nosso, %d %d ", recebido->id, recebido->n);
+						if((p==32) && (Buffer != enviado->id) && (recebido->id != enviado->id)){ //verifica se o cabeçalho possui nosso id
+							recebido->id = Buffer; //recebe o id de outrém
+							recebido->n = recebido->id & 0b111; //define que o numero de bits que serão enviados
 						}
 
-						if((p>32) && (recebido->id != enviado->id)){
-							//printk("entrou aqui sim");
-							//printk("%c", Buffer);
-							if(j < (recebido->n)){
+						if((p>32) && (recebido->id != enviado->id)){ //pega os proximos bits a partir do 32°s
+					
+							if(j < (recebido->n)){ // entra enquanto todas os caracteres nao tiverem sido recebidos
 								m++;
-								if(m==16){
-									recebido->a[j] = Buffer;
-									//printk(" %c j%dj ", recebido->a[j], j);
+								if(m==16){ // pega os proximos 32 bits lidos (m == 16 pois a contagem é feita apenas pelos bytes validos)
+									recebido->a[j] = Buffer; // preenche com o caractere recebido
 									j++;
 									m=0;
 								}
 							}
 							
-							if(j == ((recebido->n))){
-									//printk("entrei aq");
-									if((Buffer == enviado->end)){
-										reenvia = 0;
-										printk("end\n");
+							if(j == ((recebido->n))){ // caso ja tenha lido os n caracteres
+
+									if((Buffer == enviado->end)){ //
+										reenvia = 0; // reenviar não é mais necessário 
 										printk("\nestamos recebendo certo!\nRecebemos: ");
 										k_msgq_put(&msg_recebida, &(recebido->a), K_FOREVER);
 										recebido->end = Buffer;
-										recebido->U = 0;
-										recebido->sync = 0;
-										recebido->STX = 0;
+										recebido->U = 0; // zera tudo recebido no protocolo
+										recebido->sync = 0; 
+										recebido->STX = 0;  
 										recebido->id = 0;
-										recebido->end = 0;
+										recebido->a[0] = 0;
+										recebido->end = 0; 
 										j=0;
-										k_mutex_unlock(&pode_transmitir);
+										k_mutex_unlock(&pode_transmitir); // desbloqueia mutex para poder transmitir
 									}
 								}
 						}
 						
-						if(Buffer == enviado->id && (p==32) && (recebido->id != enviado->id)){
+						if(Buffer == enviado->id && (p==32) && (recebido->id != enviado->id)){ // verifica se o id é nosso (caso nós queiramos conferir nossa transmissão)
 							recebido->id = Buffer;
-							printk("id nosso");
 							p=0;
 						}
 
-						if(recebido->id == enviado->id){
+						if(recebido->id == enviado->id){ 
 							
-							recebido->n = (recebido->id) & 0b111;
-							if((Buffer == enviado->a[0]) && (p==32) && (recebido->a[0] != enviado->a[0])){
-								printk("%c", enviado->a[0]);
+							recebido->n = (recebido->id) & 0b111; // define o numero de bits que serão enviados
+							if((Buffer == enviado->a[0]) && (p==32) && (recebido->a[0] != enviado->a[0])){ //verifica se o proximo byte pós cabeçalho é igual ao primeiro caractere enviado 
 								recebido->a[0] = Buffer;
 								p=0;
 							}
 
 							if(recebido->a[j] == enviado->a[j] && (p==32)){
-								if((Buffer == enviado->a[(j+1)])){
-									printk(" %c ", enviado->a[(j+1)]);
+								if((Buffer == enviado->a[(j+1)])){ //verifica se o proximo byte pós caractere é igual ao proximo caractere enviado 
 									recebido->a[(j+1)] = Buffer;
 									j++;
 									p=0;
 								}
-								if(j == ((enviado->n)-1)){
-									printk("entrei aq");
-									if((Buffer == enviado->end)){
-										reenvia = 0;
-										printk("end\n");
+
+								if(j == ((recebido->n)-1)){ // verifica se todos os bits já foram recebidos
+									if((Buffer == enviado->end)){ //verifica se o proximo byte pós ultimo caractere é igual ao end 
+										reenvia = 0; // não é mais necessário reenviar
 										printk("\nestamos transmitindo certo!\nRecebemos: ");
-										k_msgq_put(&msg_recebida, &(recebido->a), K_FOREVER);
+										k_msgq_put(&msg_recebida, &(recebido->a), K_FOREVER); // coloca mensagem na message queue para printar na serial
 										recebido->end = Buffer;
-										recebido->U = 0;
+										recebido->U = 0; //zera tudo recebido no protocolo
 										recebido->sync = 0;
 										recebido->STX = 0;
 										recebido->id = 0;
 										recebido->a[0] = 0;
 										recebido->end = 0;
 										j=0;
-										k_mutex_unlock(&pode_transmitir);
+										k_mutex_unlock(&pode_transmitir); // libera mutex para transmitir
 									}
 								}
 							}
@@ -242,22 +228,22 @@ void seleciona_bit(int b){
 
 	struct package *enviado = &pacote_enviado;
     
-	if(b < 8){
+	if(b < 8){ //os 8 primeiros bits são do U 
 		tx = (pacote_enviado.U >> (7-b)) & 0b1;
 	}
-	else if(b < 16){
+	else if(b < 16){ //os 8 segundos bits são do sync
 		tx = (pacote_enviado.sync >> (7-(b%8))) & 0b1;
 	}
-	else if(b < 24){
+	else if(b < 24){ //os 8 terceiros bits são do STX
 		tx = (pacote_enviado.STX >> (7-(b%8))) & 0b1;
 	}
-	else if(b < 32){
-		tx = (id_random >> (7-(b%8))) & 0b1;
+	else if(b < 32){ //os 8 quartos bits são do id
+		tx = (pacote_enviado.id >> (7-(b%8))) & 0b1;
 	}
-	else if(b < (32+(8*(enviado->n)))){
+	else if(b < (32+(8*(enviado->n)))){ //os proximos bits são dos caracteres
 		tx = (pacote_enviado.a[(b/8)-4]) >> (7-(b%8)) & 0b1; // seleciona cada caractere da string e shift para o lado e faz uma mascara para enviar apenas um bit
 	}
-	else if(b <= (40+(8*(enviado->n)))){
+	else if(b <= (40+(8*(enviado->n)))){ //os 8 ultimos bits são do end
 		tx = (pacote_enviado.end >> (7-(b%8))) & 0b1;
 	}
 
@@ -267,7 +253,7 @@ void serial_cb(const struct device *dev, void *user_data)
 {
 	struct package *enviar = &pacote_enviado;
 	uint8_t c;
-	if (!uart_irq_update(uart_dev)) {
+	if (!uart_irq_update(uart_dev)) { 
 		return;
 	}
 
@@ -275,19 +261,18 @@ void serial_cb(const struct device *dev, void *user_data)
 		return;
 	}
 
-	/* read until FIFO empty */
+	/* lê até que a FIFO esteja vazia */
 	while (uart_fifo_read(uart_dev, &c, 1) == 1) {
 		if ((c == '\n' || c == '\r') && rx_buf_pos > 0) {
-			/* terminate string */
+			/* encerra string */
 			rx_buf[rx_buf_pos] = '\0';
 
-			/* if queue is full, message is silently dropped */
+			/* coloca mensagem na fila para ser transmitida */
 			k_msgq_put(&uart_msgq, &rx_buf, K_FOREVER);	
 			reenvia = 1;	
-			enviar->n = rx_buf_pos;
-			enviar->id = 0b01010000;
-			enviar->id = ((enviar->id) | enviar->n);
-			printk("\n\n\n%d %d\n\n\n", enviar->id, enviar->n);
+			enviar->n = rx_buf_pos; //define a quantidade de caracteres
+			enviar->id = 0b01010000; //define o id no cabeçalho
+			enviar->id = ((enviar->id) | enviar->n); // define todo o cabeçalho
 
 			/* reset the buffer (it was copied to the msgq) */
 			rx_buf_pos = 0;
@@ -327,13 +312,13 @@ void ler_teclado(void){
 
 	while(1){
 		if(reenvia == 0){
-			k_msgq_get(&uart_msgq, &(pacote_enviado.a), K_FOREVER);
+			k_msgq_get(&uart_msgq, &(pacote_enviado.a), K_FOREVER); // aguarda na fila até que tenha alguma mensagem para transmitir
 			printk("enviando...\n");
 		}
 
 		printk("\n");
 		
-		recebido->U = 0;
+		recebido->U = 0; // zera tudo para que ter certeza que o pacote foi enviado corretamente de uma vez só 
 		recebido->sync = 0;
 		recebido->STX = 0;
 		recebido->id = 0;
@@ -344,29 +329,32 @@ void ler_teclado(void){
 		int colisoes=0;
 		int tempo=0;
 
-		while(k_mutex_lock(&pode_transmitir, K_NO_WAIT) != 0){
-			colisoes++;
+		while(k_mutex_lock(&pode_transmitir, K_NO_WAIT) != 0){ // caso haja colisoes, espera um tempo indeterminado para transmitir
+			
+			colisoes++; // soma a quantidade de "colisões"
+
 			for(int h=0; h <= colisoes; h++){
-				tempo += sys_rand32_get();
+				tempo += sys_rand32_get(); // soma um tempo aleatorio 
 			}
+
 			k_msleep(tempo);
 		}
 
-		for(k_bit = 0; k_bit < (40+(8*(enviado->n))+2); k_bit++){
+		for(k_bit = 0; k_bit < (40+(8*(enviado->n))+2); k_bit++){ // envia todos os bits do pacote
 
-			k_condvar_wait(&mycondvar, &mymutex, K_FOREVER);
-			seleciona_bit(k_bit);
+			k_condvar_wait(&mycondvar, &mymutex, K_FOREVER); // espera um sinal para que possa alterar o valor de tx
+			seleciona_bit(k_bit); // altera o valor de tx para o proximo bit a ser enviado
 		
 		}
 		tx = 0;
-		k_mutex_unlock(&pode_transmitir);
+		k_mutex_unlock(&pode_transmitir); // desbloqueia o mutex para caso ja tenha terminado de transmitir
 	}
 }
 
 void TX(void){
 
-	k_condvar_signal(&mycondvar);
-	gpio_pin_set_dt(&pino_TX, tx);
+	k_condvar_signal(&mycondvar); // dá sinal para alterar o valor de tx
+	gpio_pin_set_dt(&pino_TX, tx); // seta o pino TX para o valor tx
 	
 }
 
@@ -375,13 +363,12 @@ void print_tela(){
 	struct package *recebido = &pacote_recebido;
 
 	char msg[7];
-	//printk("INICIALIZEI");
 
 	while(1){
-		if(k_msgq_get(&msg_recebida, &msg, K_FOREVER) == 0 ){
-			//printk("ACHEI UMA MENSAGEM de %d ", recebido->n);
-			for(int l = 0; l < (recebido->n); l++){
-				printk("%c", msg[l]);
+		if(k_msgq_get(&msg_recebida, &msg, K_FOREVER) == 0 ){ // espera para quando houver uma mensagem que foi recebida
+
+			for(int l = 0; l < (recebido->n); l++){ //printa os caracteres da mensagem em seguida
+				printk("%c", msg[l]); 
 			}
 			printk("\n");
 		}
@@ -390,17 +377,17 @@ void print_tela(){
 
 K_THREAD_DEFINE(printar_na_tela, MY_STACK_SIZE, print_tela, NULL, NULL, NULL, 1, 0, 0);
 
-K_THREAD_DEFINE(leitura_teclado, MY_STACK_SIZE, ler_teclado, NULL, NULL, NULL, MY_PRIORITY, 0, 0); // Inicializar thread que executará F1
+K_THREAD_DEFINE(leitura_teclado, MY_STACK_SIZE, ler_teclado, NULL, NULL, NULL, MY_PRIORITY, 0, 0);
 
-K_THREAD_DEFINE(Buffer_RX, MY_STACK_SIZE, RXBuffer, NULL, NULL, NULL, MY_PRIORITY, 0, 0); // Inicializar thread que executará F1
+K_THREAD_DEFINE(Buffer_RX, MY_STACK_SIZE, RXBuffer, NULL, NULL, NULL, MY_PRIORITY, 0, 0);
 
 K_TIMER_DEFINE(TX_timer, TX, NULL);
 
 K_TIMER_DEFINE(RX_timer, RX, NULL);
 
 int main(){
-	int ret = gpio_pin_configure_dt(&pino_TX, GPIO_OUTPUT_ACTIVE);
-	gpio_pin_set_dt(&pino_TX, 0);
-	k_timer_start(&TX_timer, K_MSEC(10), K_MSEC(10));
-	k_timer_start(&RX_timer, K_MSEC(10), K_USEC(2500));
+	int ret = gpio_pin_configure_dt(&pino_TX, GPIO_OUTPUT_ACTIVE); // configura o pino TX para output
+	gpio_pin_set_dt(&pino_TX, 0); // seta o pino inicialmente para zero
+	k_timer_start(&TX_timer, K_MSEC(10), K_MSEC(10)); // começa o timer para chamar a callback a cada 10ms (100bps)
+	k_timer_start(&RX_timer, K_MSEC(10), K_USEC(2500)); // começa o timer para chamar a callback a cada 2.5ms (400bps)
 }
